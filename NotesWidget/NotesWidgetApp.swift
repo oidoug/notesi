45a9d893d49739fc2a08e0221372251a4de770f4
Douglas Schmidt <github@oidoug.com>
2026-04-27 14:01:35 -0300

    Initial Commit

---
+++ NotesWidget/NotesWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesWidgetApp.swift
+//  NotesWidget
+//
+//  Created by Douglas Schmidt on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NotesWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
